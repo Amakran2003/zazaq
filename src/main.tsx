8772eac72dd15acd57e956fd05dcd7735c47c5a6
Abderrazaq MAKRAN <abderrazaq.makran@icloud.com>
2025-09-08 14:03:18 +0200

    Restore App rendering and splash logic after construction page

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import Construction from "./pages/Construction";
+import App from "./App";
 import "./index.css";
 
 // Vérifier si on vient d'une redirection de la page 404.html
@@ -11,4 +11,4 @@ if (redirectPath) {
   window.history.replaceState(null, '', redirectPath);
 }
 
-createRoot(document.getElementById("root")!).render(<Construction />);
+createRoot(document.getElementById("root")!).render(<App />);
